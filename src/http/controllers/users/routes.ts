3b61a613e31d01d8cb012ad63ada9f851e84cd81
adaobraga <adaobraga@creago.org.br>
2024-06-20 09:00:40 -0300

    implements refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,11 +5,14 @@ import { verifyJWT } from '@/http/middlewares/virify-jwt'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticate */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
